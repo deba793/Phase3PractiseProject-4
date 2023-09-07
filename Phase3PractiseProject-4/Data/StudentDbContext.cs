2eb563e75de15e3cd30c30d0cfc657bb5b2e8d1e
deba793 <debasisrout793@gmail.com>
2023-09-07 15:11:38 +0530

    Phase3PractiseProject-4

---
+++ Phase3PractiseProject-4/Data/StudentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Phase3PractiseProject_4.Models;
+
+namespace Phase3PractiseProject_4.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext (DbContextOptions<StudentDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Phase3PractiseProject_4.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ Phase3PractiseProject-4/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Phase3PractiseProject_4.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Subject { get; set; }
+        public double Mark { get; set; }
+
+    }
+}
